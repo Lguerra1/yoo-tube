e9681b7cb7585f1a25b29dea5e767d2d1d43f8ee
unknown <Nightmachine@gmail.com>
2018-12-17 15:22:08 -0700

    added iframe and videoId

--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -4,8 +4,13 @@ const VideoDetail = (props) => {
     if (!props.video) {
         return <div>Loading...</div>
     }
+
+    
     return (
         <div >
+            <div className="ui embed">
+            <iframe src={`https://www.youtube.com/embed/${props.video.id.videoId}`} />
+            </div>
             <div className="ui segment">
                 <h4 className="ui header">{props.video.snippet.title}</h4>
                 <p>{props.video.snippet.description}</p>
