d02931a639bd25d0aacb915f55429d66570ee1a8
unknown <Nightmachine@gmail.com>
2018-12-19 14:34:18 -0700

    added final touches

--- src/components/App.js
+++ src/components/App.js
@@ -9,6 +9,10 @@ export default class App extends Component {
         videos: [],
         selectedVideo: null
     }
+
+    componentDidMount(){
+      this.onTermSubmit('buildings')  
+    }
     onTermSubmit = async (term) => {
         const response = await youtube.get('/search', {
             params: {
--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -20,3 +20,5 @@ const VideoDetail = (props) => {
 }
 
 export default VideoDetail
+
+
